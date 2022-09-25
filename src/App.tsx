51f10675735e8d0fa683b8ab89d268a30dce74de
Klaus Townsend <subjunk@gmail.com>
2022-09-26 09:04:20 +1300

    Dashboard buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ActionIcon, AppShell, Button, ColorScheme, ColorSchemeProvider, Group, Header, MantineProvider } from '@mantine/core';
+import { AppShell, Button, ColorScheme, ColorSchemeProvider, Group, Header, MantineProvider, Switch, useMantineTheme } from '@mantine/core';
 import React from 'react';
-import { MoonStars, Sun } from 'tabler-icons-react';
 import { useLocalStorage } from '@mantine/hooks';
+import { IconSun, IconMoonStars } from '@tabler/icons';
 
 export default function App() {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
@@ -11,6 +11,7 @@ export default function App() {
   const toggleColorScheme = (value?: ColorScheme) => {
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   }
+  const theme = useMantineTheme();
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
@@ -23,9 +24,13 @@ export default function App() {
           padding="md"
           header={<Header height={50} p="xs">{
             <Group position="right">
-              <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
-                {colorScheme === 'dark' ? <Sun size={16} /> : <MoonStars size={16} />}
-              </ActionIcon>
+              <Switch
+                checked={colorScheme === 'dark'}
+                onChange={() => toggleColorScheme()}
+                size="lg"
+                onLabel={<IconSun color={theme.white} size={20} stroke={1.5} />}
+                offLabel={<IconMoonStars color={theme.colors.gray[6]} size={20} stroke={1.5} />}
+              />
             </Group>
           }</Header>}
           styles={(theme) => ({
