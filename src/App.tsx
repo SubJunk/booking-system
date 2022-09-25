23e45bdc74ddc3b5537e12d4b5a8fd23b2572487
SubJunk <SubJunk@users.noreply.github.com>
2022-09-25 21:33:03 +1300

    Added mantine buttons (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
+import { ActionIcon, AppShell, Button, ColorScheme, ColorSchemeProvider, Group, Header, MantineProvider } from '@mantine/core';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MoonStars, Sun } from 'tabler-icons-react';
+import { useLocalStorage } from '@mantine/hooks';
 
-function App() {
+export default function App() {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'dark',
+  });
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        <AppShell
+          padding="md"
+          header={<Header height={50} p="xs">{
+            <Group position="right">
+              <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+                {colorScheme === 'dark' ? <Sun size={16} /> : <MoonStars size={16} />}
+              </ActionIcon>
+            </Group>
+          }</Header>}
+          styles={(theme) => ({
+            main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+          })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Group>
+            <Button>New booking</Button>
+            <Button>View bookings</Button>
+          </Group>
+          <Group mt="md">
+            <Button>Manage resources</Button>
+            <Button>Manage people</Button>
+            <Button>Payment methods</Button>
+            <Button>Travel charges</Button>
+            <Button>Hourly charges</Button>
+          </Group>
+          <Group mt="md">
+            <Button>Resource sheet</Button>
+            <Button>People sheet</Button>
+            <Button>Bookings report</Button>
+          </Group>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
-
-export default App;
